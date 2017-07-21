e81b782fc458498e5d7f760c4140543596af7752
Luca Favatella <luca.favatella@erlang-solutions.com>
2017-07-21 11:00:20 +0100

    AE-134 Plant primitive load test
    Add smoke load test run as Travis CI job in order to demo how to run
    current load test scripts. The setup of basho_bench in the Travis CI
    follows
    [upstream](https://github.com/mrallen1/basho_bench/blob/c92c71c09b17f5f2c983a31d2a6ec2db40de9bfe/.travis.yml).
    
    In order to reproduce locally the smoke load test as run by CI, run in
    the shell:
    ```
    BUILD_DIR="$(pwd)"
    JOB=smokeloadtest
    
    erl -version && \
        ./ci before_install "${JOB:?}" "${BUILD_DIR:?}" && \
        ./ci install "${JOB:?}" "${BUILD_DIR:?}" && \
        ./ci before_script "${JOB:?}" "${BUILD_DIR:?}" && \
        ./ci script "${JOB:?}" "${BUILD_DIR:?}"
    ```
    
    For cleanup, run in the shell at your own risk:
    ```
    pkill beam; pkill epmd; git clean -d -x -ff .; git checkout .
    ```

---
+++ scripts/load_test/basho_bench_driver_trie.erl
@@ -0,0 +1,47 @@
+-module(basho_bench_driver_trie).
+
+-export([new/1,
+         run/4]).
+
+%% Macros compatible with header `basho_bench.hrl` - in order not to
+%% require including it for compiling this module:
+-define(INFO(Str, Args), error_logger:info_msg(Str, Args)).
+
+-record(state, {
+          trie_id,
+          root
+         }).
+
+new({_,_,Id}) ->
+    TrieId = basho_bench_config:get(trie_id),
+    KeySizeBytes = basho_bench_config:get(trie_key_size_bytes),
+    ValueSizeBytes = basho_bench_config:get(trie_value_size_bytes),
+    MetaSizeBytes = 0 = basho_bench_config:get(trie_meta_size_bytes),
+    HashSizeBytes = basho_bench_config:get(trie_hash_size_bytes),
+    Amount = basho_bench_config:get(trie_amount),
+    Mode = basho_bench_config:get(trie_mode),
+    {ok, TrieSupPid} =
+        trie_sup:start_link( %% TODO review linking
+          KeySizeBytes,
+          ValueSizeBytes,
+          TrieId,
+          Amount,
+          MetaSizeBytes,
+          HashSizeBytes,
+          Mode),
+    ?INFO("Worker ~p using trie with id ~p and sup ~p.\n", [Id, TrieId, TrieSupPid]),
+    Root = basho_bench_config:get(trie_initial_empty_root),
+    {[], _} = {trie:get_all(Root, TrieId), {{worker_id, Id}, {trie_initial_allegedly_empy_root, Root}}},
+    State = #state{trie_id = TrieId, root = Root},
+    {ok, State}.
+
+run(get, KeyGen, _ValueGen, State = #state{}) ->
+    Key = KeyGen(),
+    case trie:get(Key, State#state.root, State#state.trie_id) of
+        {_, empty, _} ->
+            {ok, State};
+        {_, _, _} ->
+            {ok, State};
+        Unknown ->
+            {error, Unknown}
+    end.
