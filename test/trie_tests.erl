3ba7e417e6e0ef9fd2bde75499b2140208c1b2d2
Luca Favatella <luca.favatella@erlang-solutions.com>
2017-07-24 15:31:38 +0100

    AE-134 Add unit tests for trie:root_hash
    Most of the test is taken from test 9 in the test_trie module.

--- test/trie_tests.erl
+++ test/trie_tests.erl
@@ -36,6 +36,113 @@ api_smoke_test_() ->
      ]
     }.
 
+root_hash_test_() ->
+    {foreach,
+     fun() ->
+	     ?debugFmt("~nCurrent working directory: ~p~n",
+		       [begin {ok, Cwd} = file:get_cwd(), Cwd end]),
+	     {ok, SupPid} =
+		 trie_sup:start_link(
+		   _KeyLength = 9,
+		   _Size = 2,
+		   _ID = ?ID,
+		   _Amount = 1000000,
+		   _Meta = 2,
+		   _HashSize = 12,
+		   _Mode = hd),
+	     ?debugFmt("~nTrie sup pid: ~p~n", [SupPid]),
+	     assert_trie_empty(0, ?ID),
+	     SupPid
+     end,
+     fun(SupPid) ->
+	     ?assert(is_process_alive(SupPid)),
+	     cleanup_alive_sup(SupPid),
+	     ?assertNot(is_process_alive(SupPid)),
+	     ok
+     end,
+     [ {"Hash of root of empty tree",
+	fun() ->
+		Root = 0,
+		RH = trie:root_hash(?ID, Root),
+		?assertMatch({RH, empty, _}, trie:get(_Key = 1, Root, ?ID)),
+		ok
+	end}
+     , {"Hash of tree root changes with tree content",
+	fun() ->
+		Root = 0,
+		RH = trie:root_hash(?ID, Root),
+		Key = 1,
+		Root2 = trie:put(Key, _V = <<1,1>>, _Meta = 0, Root, ?ID),
+		RH2 = trie:root_hash(?ID, Root2),
+		?assertMatch({RH2, _, _}, trie:get(Key, Root2, ?ID)),
+		?assertNotEqual(RH, RH2),
+		ok
+	end}
+     , {"Hash of tree root changes with leaf key",
+	fun() ->
+		Root = 0,
+		K1 = 1,
+		K2 = 2,
+		V = <<1,1>>,
+		Meta = 0,
+		Root2A = trie:put(K1, V, Meta, Root, ?ID),
+		Root2B = trie:put(K2, V, Meta, Root, ?ID),
+		?assertNotEqual(trie:root_hash(?ID, Root2A),
+				trie:root_hash(?ID, Root2B)),
+		ok
+	end}
+     , {"Hash of tree root changes with leaf value",
+	fun() ->
+		Root = 0,
+		K = 1,
+		V1 = <<1,1>>,
+		V2 = <<1,2>>,
+		Meta = 0,
+		Root2A = trie:put(K, V1, Meta, Root, ?ID),
+		Root2B = trie:put(K, V2, Meta, Root, ?ID),
+		?assertNotEqual(trie:root_hash(?ID, Root2A),
+				trie:root_hash(?ID, Root2B)),
+		ok
+	end}
+     , {"Hash of tree root does not change with leaf meta",
+	fun() ->
+		Root = 0,
+		K = 1,
+		V = <<1,1>>,
+		Meta1 = 0,
+		Meta2 = 1,
+		Root2A = trie:put(K, V, Meta1, Root, ?ID),
+		Root2B = trie:put(K, V, Meta2, Root, ?ID),
+		?assertEqual(trie:root_hash(?ID, Root2A),
+			     trie:root_hash(?ID, Root2B)),
+		ok
+	end}
+     , {"Hash of tree root depends on tree content rather than order of operations - case empty tree",
+	fun() ->
+		Root = 0,
+		Key = 1,
+		Root2 = trie:put(Key, _V = <<1,1>>, _Meta = 0, Root, ?ID),
+		Root3 = trie:delete(Key, Root2, ?ID),
+		?assertEqual(trie:root_hash(?ID, Root),
+			     trie:root_hash(?ID, Root3)),
+		ok
+	end}
+     , {"Hash of tree root depends on tree content rather than order of operations - case non-empty tree",
+	fun() ->
+		Root = 0,
+		K1 = 1,
+		K2 = 2,
+		Meta = 0,
+		Root2 = trie:put(K1, _V1 = <<1,1>>, Meta, Root, ?ID),
+		Root3 = trie:put(K2, _V2 = <<1,2>>, Meta, Root2, ?ID),
+		Root4 = trie:delete(K2, Root3, ?ID),
+		?assertEqual(trie:root_hash(?ID, Root2),
+			     trie:root_hash(?ID, Root4)),
+		ok
+	end}
+     ]
+    }.
+
 key_range_good_test_() ->
     {foreach,
      fun() ->
