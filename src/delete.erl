3f211206526d117da1f2ba909fac96085d742d47
Luca Favatella <luca.favatella@erlang-solutions.com>
2017-07-25 17:59:03 +0100

    AE-134 Fix deletion of non-existent key
    ... from tree containing a key with same path prefix.

--- src/delete.erl
+++ src/delete.erl
@@ -3,11 +3,13 @@
 
 delete(ID, Root, CFG) ->
     Path = leaf:path_maker(ID, CFG),
-    Branch = store:get_branch(Path, 0, Root, [], CFG),
-    %{_, Leaf, Proof} = get:get(Path, Root, CFG),
-    X = cfg:hash_size(CFG)*8,
-    %X = hash:hash_depth()*8,
-    EmptyHash = <<0:X>>,
-    {_, NewRoot, _} = store:store_branch(Branch, Path, 0, 0, EmptyHash, CFG),
-    NewRoot.
+    case store:get_branch(Path, 0, Root, [], CFG) of
+	{_, _, _} -> %already no leaf with the specified path
+	    Root;
+	Branch ->
+	    X = cfg:hash_size(CFG)*8,
+	    EmptyHash = <<0:X>>,
+	    {_, NewRoot, _} = store:store_branch(Branch, Path, 0, 0, EmptyHash, CFG),
+	    NewRoot
+    end.
 
