feabbfd5375ccad23991e9e1d45c3daca88d0f3e
zack-bitcoin <zack.bitcoin@gmail.com>
2020-01-24 19:40:43 -0300

    don't turn off non-existant dump child processes

--- src/trie_sup.erl
+++ src/trie_sup.erl
@@ -9,11 +9,11 @@ start_link(KeyLength, Size, ID, Amount, Meta, HashSize, Mode, Location) ->
 stop(ID) -> 
     CFG = trie:cfg(ID),
     supervisor:terminate_child({global, ID}, ids:main(CFG)),
-    dump_sup:stop(ids:stem(CFG)),
-    supervisor:terminate_child({global, ID}, ids:stem(CFG)),
-    dump_sup:stop(ids:leaf(CFG)),
-    supervisor:terminate_child({global, ID}, ids:leaf(CFG)),
-    supervisor:terminate_child({global, ID}, ids:bits(CFG)),
+    %dump_sup:stop(ids:stem(CFG)),
+    %supervisor:terminate_child({global, ID}, ids:stem(CFG)),
+    %dump_sup:stop(ids:leaf(CFG)),
+    %supervisor:terminate_child({global, ID}, ids:leaf(CFG)),
+    %supervisor:terminate_child({global, ID}, ids:bits(CFG)),
     ok.
 
 %trie01_main).
