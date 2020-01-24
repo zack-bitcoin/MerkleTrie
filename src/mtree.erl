4c4a187799775b235d8422869d30d46e5c7b77bc
zack-bitcoin <zack.bitcoin@gmail.com>
2020-01-24 12:35:07 -0300

    wrote mtree, a merkel tree that doesn't depend on the dump process, and doesn't need to be a named process.

--- src/mtree.erl
+++ src/mtree.erl
@@ -1,5 +1,6 @@
 -module(mtree).
--export([test/0]).
+-export([new_empty/3, new_restoration/4, save_to_file/2, load_from_file/1, clean/2, garbage/3, restore/5, get/3, get_all/2, root_hash/2,
+	 test/0]).
 
 -record(mt, {cfg, ets, top}).
 -define(ID, unnamed).
@@ -48,23 +49,18 @@ ets_update(M, L, D) ->
 %%%%%
 
 %Type can be `stem` or `leaf`
-
-
 element_get(Type, Pointer, M) ->
     true = Pointer > 0,
     S = ets_read(M, Pointer),
     CFG = M#mt.cfg,
     Type:deserialize(S, CFG).
-    
 element_update(Type, Loc, Stem, M) ->
     CFG = M#mt.cfg,
     ets_update(M, Loc, Type:serialize(Stem, CFG)).
 element_write(Type, Stem, M) ->
     CFG = M#mt.cfg,
     ets_write(M, Type:serialize(Stem, CFG)).
-    
 element_write_batch(Things, M) ->
-    %SL = stem:serialize_stems(Leaves, CFG),
     SL = serialize_list(Things, M),
     ets_write_batch(M, SL).
 serialize_list([], _) -> [];
@@ -93,20 +89,15 @@ new(KeyLength, Size, Meta) ->
 	    32, %HashSize
 	    ram),
     #mt{cfg = CFG, ets = P, top = 1}.
-
-
 new_empty(KeyLength, Size, Meta) ->
     M = new(KeyLength, Size, Meta),
     Stem = stem:new_empty(M#mt.cfg),
     {M2, _} = element_write(stem, Stem, M),
     M2.
-
 new_restoration(RootStem, KeyLength, Size, Meta) ->
     M = new(KeyLength, Size, Meta),
     element_write(stem, RootStem, M).
-
 top(M) -> M#mt.top.
-
 loc2rest(Loc) ->
     {F, _} = lists:split(length(Loc) - 3, Loc),
     Loc2 = F ++ "_rest.db".
@@ -116,12 +107,11 @@ save_to_file(M, Loc) ->
     ets:tab2file(M#mt.ets, Loc, [{sync, true}]).
 load_from_file(Loc) ->
     Loc2 = loc2rest(Loc),
-    M = db:read(loc2rest(Loc)),
+    M = binary_to_term(db:read(loc2rest(Loc))),
     {ok, P} = ets:file2tab(Loc),
     M#mt{ets = P}.
-
 clean(M, Pointer) ->
-    {ok, ET} = ets_new_db(),
+    ET = ets_new_db(),
     CFG = M#mt.cfg,
     RootStem = element_get(stem, Pointer, M),
     {NewM, _} = new_restoration(RootStem, 
@@ -131,14 +121,12 @@ clean(M, Pointer) ->
     {_, NewM2} = clean2(Pointer, M, NewM, CFG),
     ets_delete_db(M),
     NewM2.
-
 clean2(Pointer, M, NewM, CFG) ->
     S = element_get(stem, Pointer, M),
     P = tuple_to_list(stem:pointers(S)),
     T = tuple_to_list(stem:types(S)),
     H = tuple_to_list(stem:hashes(S)),
     NewM2 = clean3(P, T, H, M, NewM, CFG),
-    %SS = stem:serialize(S, CFG),
     element_update(stem, Pointer, S, NewM2),
     {stem:hash(S, CFG), NewM2}.
 clean3([],[],_,_,M,_) -> M;
@@ -155,8 +143,8 @@ clean3([Pointer|PT],
 		 M2;
 	     2 -> %leaf
 		 Leaf = element_get(leaf, Pointer, M),
-		 {M2, _} = element_update(leaf, Pointer, Leaf, NewM),
-		 M2
+		 element_update(leaf, Pointer, Leaf, NewM),
+		 NewM
 	 end,
     clean3(PT, TT, HT, M, M3, CFG).
 
@@ -167,7 +155,7 @@ garbage(Trash, Keep, M) -> %returns {M, List}
     M2 = if
 	     Trash == Keep -> M;
 	     true -> 
-		 ets:delete(M, Trash)
+		 ets_delete(M, Trash)
 	 end,
     garbage2(M2, TStem, KStem, 1).
 garbage2(_,_,_,17) -> ok;
@@ -190,9 +178,7 @@ garbage2(M, S1, S2, N) ->
 		 end
 	 end,
     garbage2(M2, S1, S2, N+1).
-
 restore(Leaf, Hash, Proof, Pointer, M) ->	    
-    %Hash = root_hash(Pointer, M),
     CFG = M#mt.cfg,
     HSE = cfg:hash_size(CFG) * 8,
     true = verify:proof(Hash, Leaf, Proof, CFG),
@@ -221,7 +207,6 @@ restore(Leaf, Hash, Proof, Pointer, M) ->
     Branch2 = get_branch(Path, 0, Pointer, [], M3),%branch2 proves everything else.
     Branch3 = store:combine_branches(Branch, Branch2),
     store_branch(Branch3, Path, Type, LPointer, LH, M3).
-    %restore2(Path, Proof, Type, LPointer, LH, M2).
 store_branch([], Path, _, Pointer, _, M) ->
     %Instead of getting the thing, we can build it up while doing store.
     {Hash, _, Proof} = get(Path, Pointer, M),
@@ -240,7 +225,6 @@ get_branch(Path, N, Parent, Trail, Mt) ->
     CFG = Mt#mt.cfg,
     M = N+1,
     <<A:4>> = lists:nth(M, Path), 
-    %R = stem:get(Parent, CFG),
     R = element_get(stem, Parent, Mt),
     Pointer = stem:pointer(A+1, R),
     RP = [R|Trail],
@@ -250,7 +234,6 @@ get_branch(Path, N, Parent, Trail, Mt) ->
 	Pointer == 0 -> RP;
 	ST == 1 -> get_branch(Path, M, Pointer, RP, CFG);
 	ST == 2 ->
-	    %Leaf = leaf:get(Pointer, CFG),
 	    Leaf = element_get(leaf, Pointer, Mt),
 	    case leaf:path(Leaf, CFG) of
 		Path -> %overwrite
@@ -265,22 +248,18 @@ proof2branch([H|T], _, _, Hash, _, M, R) when is_binary(H) ->
     CFG = M#mt.cfg, 
     L = leaf:deserialize(H, CFG),
     Path = leaf:path(L, CFG),
-    %Pointer = leaf:put(L, CFG),
     {M2, Pointer} = element_write(leaf, L, M),
     proof2branch(T, 2, Pointer, Hash, Path, M2, R);
 proof2branch([H|T], Type, Pointer, Hash, Path, M,R) -> 
     CFG = M#mt.cfg, 
     [<<Nibble:4>> | NewPath] = Path,
     S = stem:recover(Nibble, Type, Pointer, Hash, H, CFG),
-    %NewPointer = stem:put(S, CFG),
     {M2, NewPointer} = element_write(stem, S, M),
     NewHash = stem:hash(S, CFG),
     proof2branch(T, 1, NewPointer, NewHash, NewPath, M, [S|R]).
-%[S|proof2branch(T, 1, NewPointer, NewHash, NewPath, CFG)].
 
 store_batch(Leaves, Root, M) ->
     CFG = M#mt.cfg,
-    %Leaves2 = store:sort_by_path(Leaves, CFG),
     Leaves2 = lists:sort(fun(A, B) ->
 				 KeyA = leaf:path_maker(leaf:key(A), CFG),
 				 KeyB = leaf:path_maker(leaf:key(B), CFG),
@@ -298,20 +277,6 @@ store_batch(Leaves, Root, M) ->
     StemTop = M2#mt.top,
     {FHash, FLoc, SToStore} = store:batch2_ram(BStart, BranchData, CFG, StemTop, []),
     F = fun({P, _}) -> P end,
-    io:fwrite("top: "),
-    io:fwrite(integer_to_list(Top)),
-    io:fwrite("\nstemtop: "),
-    io:fwrite(integer_to_list(StemTop)),
-    io:fwrite("\nhd(tostore): "),
-    io:fwrite(integer_to_list(F(hd(ToStore)))),
-    io:fwrite("\nlength(tostore): "),
-    io:fwrite(integer_to_list(length(ToStore))),
-    io:fwrite("\nhd(stemstore): "),
-    io:fwrite(integer_to_list(F(hd(SToStore)))),
-    io:fwrite("\n"),
-    %io:fwrite({Top, StemTop,
-	%       lists:map(F, ToStore), 
-	%       lists:map(F, SToStore)}),
     M3 = element_write_batch(SToStore, M2),%write leaves.
     {FLoc, M3}.
 store_batch_helper_ram([], _, X, _, _Pointer, L) ->
@@ -329,12 +294,10 @@ store_batch_helper_ram([H|T], M, BD, Root, Pointer, L) ->
 		          store_batch_helper_ram(T, M, [{0, <<0:X>>, Path, Branch0, 0}|BD], Root, Pointer, L)
 			      end;
        _ ->
-	       %NLP = leaf:put(H, CFG),
 	       NLH = leaf:hash(H, CFG),
 	       {Br, NewPointer, L2} = 
 	       case GB of
 		      {Leaf2, _LP1, Branch} ->%split leaf, add stem(s)
-		      %LP2 = leaf:put(Leaf2, CFG),
 		      %need to add 1 or more stems.
 		       {A, N2} = store:path_match(Path, leaf:path(Leaf2, CFG)),
 		       [Hp|Tp] = store:empty_stems(max(1, A-length(Branch)+1), CFG),
@@ -346,8 +309,6 @@ store_batch_helper_ram([H|T], M, BD, Root, Pointer, L) ->
 			   end,
 	    store_batch_helper_ram(T, M, [{Pointer, NLH, Path, Br, 2}|BD], Root, NewPointer, L2)
     end.
-
-
 get(Path, Root, M) ->
     CFG = M#mt.cfg,
     S = element_get(stem, Root, M),
@@ -363,7 +324,6 @@ get2([<<N:4>> | Path], Stem, Proof, M) ->
     PN = stem:pointer(N+1, Stem),
     if
 	NextType == 0 -> %empty
-	    %Next = stem:get(PN, CFG),
 	    {empty, Proof};
 	PN == 0 -> {unknown, Proof};
 	NextType == 1 -> %another stem
@@ -384,8 +344,6 @@ get2([<<N:4>> | Path], Stem, Proof, M) ->
     end.
 
 get_all(Pointer, M) ->
-    %S = stem:get(Pointer, CFG),
-    %S = ets_read(M, RootPointer),
     S = element_get(stem, Pointer, M),
     P = tuple_to_list(stem:pointers(S)),
     T = tuple_to_list(stem:types(S)),
@@ -396,7 +354,7 @@ get_all_internal2([A|AT], [T|TT], M) ->
     B = case T of
 	    0 -> [];%empty
 	    1 -> get_all(A, M);%another
-	    2 -> [element_get(leaf, A, M)]%leaf:get(A, CFG)]%a leaf
+	    2 -> [element_get(leaf, A, M)]%a leaf
 	end,
     B++get_all_internal2(AT, TT, M).
 
@@ -415,41 +373,36 @@ test() ->
     CFG = M#mt.cfg,
     Leaf1 = leaf:new(1, V1, Meta, CFG),
     Leaf2 = leaf:new(2, V2, Meta, CFG),
-    %RH0 = root_hash(0, M),
     Leaves = [Leaf1, Leaf2],
     {Root, M2} = store_batch(Leaves, 1, M),
     Path = leaf:path(Leaf1, CFG),
     Path2 = leaf:path(Leaf2, CFG),
     {Hash1, Leaf1, Proof} = get(Path, Root, M2),
     {Hash1, Leaf2, Proof} = get(Path2, Root, M2),
-    %X = get_all(Root, M2),
     true = verify:proof(Hash1, Leaf1, Proof, CFG),
-    %RootStem = ets_read(M2, Root),
     RootStem = element_get(stem, Root, M2),
-    %io:fwrite(RootStem),
     {M3, Root3} = new_restoration(
-	   %stem:deserialize(RootStem, CFG), 
 		     RootStem,
 		     5, 2, 0),
-    %io:fwrite(Root3),
     true = verify:proof(Hash1, Leaf1, Proof, CFG),
     {Hash1, _, Proof, M4} = restore(Leaf1, Hash1, Proof, Root3, M3),
     {Hash1, Leaf1, Proof} = get(Path, Root3, M4),
 
     Leaf3 = leaf:new(3, V3, Meta, CFG),
     {Root2, M5} = store_batch([Leaf3], Root, M2),
-    %[Path, leaf:path(Leaf3, CFG)].
     
-    %garbage(Root, Root2, M4),
-    %M4.
-    get(leaf:path(Leaf2, CFG), Root2, M5).
-    %ok.
-    %TODO
-    %test garbage
-    %passing test of get_all
-    %saving to file, and loading from file
-    %clean
-    %success.
+    get(leaf:path(Leaf2, CFG), Root, M5),
+    garbage(Root, Root2, M5),
+    %get(leaf:path(Leaf2, CFG), Root, M5), does not work after garbage collect
+    LFC = get(leaf:path(Leaf2, CFG), Root2, M5),
+    [Leaf1, Leaf2, Leaf3] = get_all(Root2, M5),
 
-    %ets:insert(M#mt.ets, {
-%ok.
+    File = "temp.db",
+    save_to_file(M5, File),
+    M6 = load_from_file(File),
+    LFC = get(leaf:path(Leaf2, CFG), Root2, M6),
+
+    M7 = clean(M5, Root2),
+    LFC = get(leaf:path(Leaf2, CFG), Root2, M7),
+
+    success.
